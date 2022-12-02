7f1ef13e323c63b6a800248b28a6ea14deec9588
Maksims Slotovs <maksims.slotovs@printful.com>
2022-12-02 11:18:40 +0200

    day 2 part 2

--- day2/main.go
+++ day2/main.go
@@ -24,12 +24,31 @@ var gMap = map[string]map[string]int{
 	},
 }
 
+var actionMap = map[string]map[string]int{
+	"A": {
+		"X": 0 + 3,
+		"Y": 3 + 1,
+		"Z": 6 + 2,
+	},
+	"B": {
+		"X": 0 + 1,
+		"Y": 3 + 2,
+		"Z": 6 + 3,
+	},
+	"C": {
+		"X": 0 + 2,
+		"Y": 3 + 3,
+		"Z": 6 + 1,
+	},
+}
+
 func Day2() {
 	file, _ := os.ReadFile("day2/input.txt")
 	input := string(file)
 	list := strings.Split(input, "\n")
 
 	score := 0
+	secondScore := 0
 
 	for _, s := range list {
 		if s == "" {
@@ -38,7 +57,9 @@ func Day2() {
 
 		picks := strings.Split(s, " ")
 		score += gMap[picks[0]][picks[1]]
+		secondScore += actionMap[picks[0]][picks[1]]
 	}
 
 	fmt.Println("total score: ", score)
+	fmt.Println("total second score: ", secondScore)
 }
