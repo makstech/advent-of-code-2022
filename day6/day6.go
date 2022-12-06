23ffc645968c1612bb2c6f898e011cacab17b933
Maksims Slotovs <maksims.slotovs@printful.com>
2022-12-06 10:01:01 +0200

    day 6 part 2

--- day6/day6.go
+++ day6/day6.go
@@ -11,17 +11,29 @@ func Day6() {
 	input := string(file)
 	list := strings.Split(input, "")
 	var startOfPacket int
+	var startOfPacket14 int
 
 	for i := 4; i < len(list); i++ {
-		chars := map[string]bool{}
-		for _, c := range list[i-4 : i] {
-			chars[c] = true
-		}
-		if len(chars) == 4 {
-			startOfPacket = i
-			break
+		if startOfPacket == 0 {
+			chars := map[string]bool{}
+			for _, c := range list[i-4 : i] {
+				chars[c] = true
+			}
+			if len(chars) == 4 {
+				startOfPacket = i
+			}
+		} else {
+			chars14 := map[string]bool{}
+			for _, c := range list[i-14 : i] {
+				chars14[c] = true
+			}
+			if len(chars14) == 14 {
+				startOfPacket14 = i
+				break
+			}
 		}
 	}
 
 	fmt.Println("characters processed before the first start-of-packet marker is detected:", startOfPacket)
+	fmt.Println("characters processed before the first start-of-message marker is detected:", startOfPacket14)
 }
