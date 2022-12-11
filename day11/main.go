10a1ef02ba3acc09b0e22bfaaec3d563ba5cccd3
Maksims Slotovs <maksims.slotovs@printful.com>
2022-12-12 00:50:29 +0200

    day 11 part 2

--- day11/main.go
+++ day11/main.go
@@ -9,21 +9,24 @@ import (
 	"strings"
 )
 
+type Monkey struct {
+	items     []int
+	operation struct {
+		action string
+		second int
+		old    bool
+	}
+	division  int
+	throw     map[bool]int
+	inspected int
+}
+
 func Day11() {
 	file, _ := os.ReadFile("day11/input.txt")
 	list := strings.Split(string(file), "\n\n")
-	type Monkey struct {
-		items     []int
-		operation struct {
-			action string
-			second int
-			old    bool
-		}
-		division  int
-		throw     map[bool]int
-		inspected int
-	}
 	var monkeys []Monkey
+	var monkeys2 []Monkey
+	allTest := 1
 	num := regexp.MustCompile("[0-9]+")
 	for _, m := range list {
 		monkey := Monkey{items: []int{}, throw: map[bool]int{}}
@@ -41,41 +44,56 @@ func Day11() {
 			monkey.operation.second = secondI
 		}
 		fmt.Sscanf(rows[3], "  Test: divisible by %d", &monkey.division)
+		allTest *= monkey.division
 		var to int
 		fmt.Sscanf(rows[4], "    If true: throw to monkey %d", &to)
 		monkey.throw[true] = to
 		fmt.Sscanf(rows[5], "    If false: throw to monkey %d", &to)
 		monkey.throw[false] = to
 		monkeys = append(monkeys, monkey)
-	}
-	for _, monkey := range monkeys {
-		fmt.Println(monkey)
+		monkeys2 = append(monkeys2, monkey)
 	}
 	for r := 0; r < 20; r++ {
-		for i := 0; i < len(monkeys); i++ {
-			monkey := &monkeys[i]
-			for _, item := range monkey.items {
-				second := monkey.operation.second
-				if monkey.operation.old {
-					second = item
-				}
-				var stress int
-				switch monkey.operation.action {
-				case "+":
-					stress = item + second
-				case "*":
-					stress = item * second
-				}
-				stress = stress / 3
-				throwTo := monkey.throw[stress%monkey.division == 0]
-				monkeys[throwTo].items = append(monkeys[throwTo].items, stress)
-			}
-			monkey.inspected += len(monkey.items)
-			monkey.items = []int{}
-		}
+		MonkeyRound(monkeys, true, allTest)
 	}
+	fmt.Println("level of monkey business after 20 rounds of stuff-slinging simian shenanigans:", SortAndTopLevel(monkeys))
+	for r := 0; r < 10000; r++ {
+		MonkeyRound(monkeys2, false, allTest)
+	}
+	fmt.Println(monkeys2)
+	fmt.Println("level of monkey business after 10000 rounds of stuff-slinging simian shenanigans:", SortAndTopLevel(monkeys2))
+}
+
+func MonkeyRound(monkeys []Monkey, relief bool, allTest int) {
+	for i := 0; i < len(monkeys); i++ {
+		monkey := &monkeys[i]
+		for _, item := range monkey.items {
+			second := monkey.operation.second
+			if monkey.operation.old {
+				second = item
+			}
+			var stress int
+			switch monkey.operation.action {
+			case "+":
+				stress = item + second
+			case "*":
+				stress = item * second
+			}
+			if relief {
+				stress = stress / 3
+			}
+			stress = stress % allTest
+			throwTo := monkey.throw[stress%monkey.division == 0]
+			monkeys[throwTo].items = append(monkeys[throwTo].items, stress)
+		}
+		monkey.inspected += len(monkey.items)
+		monkey.items = []int{}
+	}
+}
+
+func SortAndTopLevel(monkeys []Monkey) int {
 	sort.Slice(monkeys, func(i, j int) bool {
 		return monkeys[i].inspected > monkeys[j].inspected
 	})
-	fmt.Println("level of monkey business after 20 rounds of stuff-slinging simian shenanigans:", monkeys[0].inspected*monkeys[1].inspected)
+	return monkeys[0].inspected * monkeys[1].inspected
 }
